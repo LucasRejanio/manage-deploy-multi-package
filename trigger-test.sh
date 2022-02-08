b011485a16860006df09a814c5a3e2423d7b641e
Lucas Rejanio <lucas.rejanio@enjoei.com.br>
2022-02-08 16:32:15 -0300

    test(actions): adjusting curl command

--- trigger-test.sh
+++ trigger-test.sh
@@ -19,9 +19,14 @@ echo "=== Starting script to trigger s3 deploy with github actions ==="
 
 # Call actions to start deploy
 echo "Send request to trigger action"
-status_code=$(curl --write-out '%{http_code}' -u "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:" -X POST -H 'Accept: application/vnd.github.v3+json' \
+status_code=$(curl --write-out '%{http_code}' -u "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:" -X POST -H 'Accept: application/vnd.github.v3+json' \
 https://api.github.com/repos/LucasRejanio/manage-deploy-multi-package/actions/workflows/deploys.yml/dispatches \
--d '{"ref":"main","inputs":{"envionment":"test","bucket":"test","package:test"}}')
+-d '{
+    "ref":"main",
+    "inputs":{
+        "envionment":"test"
+    }
+}')
 
 if [[ "$status_code" == 204 ]] ; then
     echo "$status_code Successful request"
