ca5cb548b5ec12f9aa2d90c689d0337d9d9aa571
Lucas Rejanio <lucas.rejanio@enjoei.com.br>
2022-02-09 12:23:29 -0300

    fix(request): changing token access

--- trigger-test.sh
+++ trigger-test.sh
@@ -17,15 +17,11 @@ echo "=== Starting script to trigger s3 deploy with github actions ==="
 # TOKEN=$(aws --profile ci ssm get-parameters --region us-east-1 --names /github/api_token \
 #         --with-decryption --query "Parameters[0].Value" | tr -d '"')
 
-echo $REF
-
-PAYLOAD='{"ref":"'"$REF"'","inputs":{"environment":"'"$ENVIRONMENT"'", "bucket":"'"$BUCKET"'", "packages":"'"$PACKAGE"'"}}'
-
 # Call actions to start deploy
 echo "Send request to trigger action"
-status_code=$(curl --write-out '%{http_code}' -u "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:" -X POST -H 'Accept: application/vnd.github.v3+json' \
+status_code=$(curl --write-out '%{http_code}' -u "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:" -X POST -H 'Accept: application/vnd.github.v3+json' \
 https://api.github.com/repos/LucasRejanio/manage-deploy-multi-package/actions/workflows/deploys.yml/dispatches \
--d $PAYLOAD)
+-d '{"ref":"'"$REF"'","inputs":{"environment":"'"$ENVIRONMENT"'", "bucket":"'"$BUCKET"'"}}')
 
 if [[ "$status_code" == 204 ]] ; then
     echo "$status_code Successful request"
